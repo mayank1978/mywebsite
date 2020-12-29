47bc4db8de9ff6cff14fb56c221a7be283b5e8c9
mayank1978 <73350720+mayank1978@users.noreply.github.com>
2020-12-29 22:47:12 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', today = date.today(), user_name = 'aditya')
+
+@app.route('/<page_name>')
+def about(page_name):
+    return render_template(f'{page_name}.html.css.static')
+
+@app.route('/you/<string:user_name>')
+def youtube(user_name):
+    return redirect(f'https://www.youtube.com/c/{user_name}')
+
