07314602c0ea46d0f709d72c90e372809b0243e6
mayank1978 <73350720+mayank1978@users.noreply.github.com>
2021-01-05 22:43:22 +0530

    adding contact form

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,request
 from datetime import date
 
 app = Flask(__name__)
@@ -16,3 +16,14 @@ def about(page_name):
 def youtube(user_name):
     return redirect(f'https://www.youtube.com/c/{user_name}')
 
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    data = request.form.to_dict()
+    print(data)
+    with open('database.txt','a') as file:
+        name=data["name"]
+        email=data["email"]
+        message=data["message"]
+        file.write(f"\n{name},{email},{message}")
+    return render_template('thankyou.html', name = name)
+    
